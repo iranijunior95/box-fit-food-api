dc913618345311533eb52e0f6bce16f02e0a603c
irani junior <irani-junior@irani-junior-Inspiron-3442.(none)>
2024-12-05 19:37:14 -0300

    commit: validações de vendas

--- src/middlewares/vendas.middleware.js
+++ src/middlewares/vendas.middleware.js
@@ -12,7 +12,55 @@ function validate(req, res, next) {
     
     //Valida campo valor total
     const validaValorTotal = validarCamposMonetarios(valor_total, 'valor_total', 'Valor Total');
-    validaValorTotal.length !== 0 ? errors.push(validaValorTotal[0]) : null; 
+    validaValorTotal.length !== 0 ? errors.push(validaValorTotal[0]) : null;
+    
+    //Valida campo pagamento
+    if(pagamento === undefined) {
+        errors.push({ campo: 'pagamento', mensagem: 'O campo "Pagamento" é obrigatorio...'});
+    }
+
+    if(pagamento === '') {
+        errors.push({ campo: 'pagamento', mensagem: 'O campo "Pagamento" não pode ser vazio...'});
+    }
+
+    //Valida campo estado
+    if(estado === undefined) {
+        errors.push({ campo: 'estado', mensagem: 'O campo "Estado Pagamento" é obrigatorio...'});
+    }
+
+    if(estado === '') {
+        errors.push({ campo: 'estado', mensagem: 'O campo "Estado Pagamento" não pode ser vazio...'});
+    }
+
+    //Valida campo id cliente
+    if(cliente === undefined) {
+        errors.push({ campo: 'cliente', mensagem: 'O campo "Cliente" é obrigatorio...'});
+    }
+
+    if(cliente === '') {
+        errors.push({ campo: 'cliente', mensagem: 'O campo "Cliente" não pode ser vazio...'});
+    }
+
+    if(cliente !== '' & cliente !== undefined & isNaN(cliente)) {
+        errors.push({ campo: 'cliente', mensagem: 'O campo "Cliente" precisa ser um valor númerico...'});
+    }
+
+    //Valida campo lista de produtos
+    if(produtos === undefined) {
+        errors.push({ campo: 'produtos', mensagem: 'O campo "Produtos" é obrigatorio...'});
+    }
+
+    if(produtos === '') {
+        errors.push({ campo: 'produtos', mensagem: 'O campo "Produtos" não pode ser vazio...'});
+    }
+
+    if(produtos !== '' & produtos !== undefined & !Array.isArray(produtos)) {
+        errors.push({ campo: 'produtos', mensagem: 'O campo "Produtos" precisa ser uma lista...'});
+    }
+
+    if(produtos !== '' & produtos !== undefined & Array.isArray(produtos) & produtos.length === 0) {
+        errors.push({ campo: 'produtos', mensagem: 'O campo "Produtos" não pode ser uma lista vazia...'});
+    }
 
     return errors.length === 0 ? next() : res.status(400).json({ status: false, errors });
 }
